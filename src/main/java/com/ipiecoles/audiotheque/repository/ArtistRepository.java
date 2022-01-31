e5a6b19df002d8ddbe976cab8c4e3b4a58ab90fc
Arthur Montesinos <60731021+voxars@users.noreply.github.com>
2022-01-31 22:34:52 +0100

    -

--- src/main/java/com/ipiecoles/audiotheque/repository/ArtistRepository.java
+++ src/main/java/com/ipiecoles/audiotheque/repository/ArtistRepository.java
@@ -11,33 +11,24 @@ import java.util.List;
 import java.util.Set;
 
 public interface ArtistRepository extends JpaRepository<Artist, Long> {
-    //Artist findById(int id);
-    //@Query("select * from artist where id = " + unId);
+
     boolean existsByName(String name);
 
-    //Artist findArtistById(Integer id);
+
 
     @Query(value = "SELECT * FROM artist a WHERE a.name LIKE :name% ", nativeQuery = true)
-    List<Artist> findByName(@Param("name") String name); //On met dans le paramètre du nom raccourci dans la requète  ex :'aero%'
+    List<Artist> findByName(@Param("name") String name); 
 
 
 
     @Query(value = "SELECT * FROM artist a WHERE a.name LIKE ?1", nativeQuery = true)
     Artist findByNameOnly(String name);
 
-    /*
-    @Query(value = "SELECT art.id,art.name,alb.title FROM artist art INNER JOIN album alb ON art.id = alb.artist_id WHERE artist_id;", nativeQuery = true)
-    Set<Album> getAllAlbumsByArtist(Long artist_id);*/
-
     @Modifying
     @Query(value = "DELETE FROM artist WHERE id = ?1 ;", nativeQuery = true)
     void deleteArtistById(Long artiste_id);
 
-    //Boolean existsById(Long id);
-    //Artist findById(Integer id);
 
-    /*@Query(value = "SELECT MAX(id) FROM artist;", nativeQuery = true)
-    Long findLastId();*/
 
 
 
